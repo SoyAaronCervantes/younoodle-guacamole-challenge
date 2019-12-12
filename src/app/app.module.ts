290e8845ec230a375fd015842c197a0e0406ae3f
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2019-12-12 02:36:11 -0600

    Added componentes to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { InvestorCardComponent } from './component/investor-card/investor-card.component';
+import { ListStartupComponent } from './component/list-startup/list-startup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InvestorCardComponent,
+    ListStartupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
